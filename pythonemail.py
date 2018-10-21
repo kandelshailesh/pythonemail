5e90767b4a568315e49d1d4fb5774a3216bac114
kandelshailesh <shaileshkandel123@gmail.com>
2018-10-21 23:05:18 +0545

    email

---
+++ pythonemail.py
@@ -0,0 +1,25 @@
+import smtplib
+
+# Import the email modules we'll need
+from email.message import EmailMessage
+
+# Open the plain text file whose name is in textfile for reading.
+with open('firrs.txt') as fp:
+    # Create a text/plain message
+    msg = EmailMessage()
+    msg.set_content(fp.read())
+me ='shaileshkandel123@gmail.com'
+you ='shaileshkandel123@gmail.com'
+msg['subject'] = 'The contents of firrs.txt'
+msg['From'] = me
+msg['To'] = you
+# Send the message via our own SMTP server.
+print(msg)
+s = smtplib.SMTP('smtp.gmail.com',587)
+s.ehlo()
+s.starttls()
+s.login(me,'sabitra54321')
+#s.sendmail(me,you,msg)
+s.send_message(msg)
+s.quit()
+
